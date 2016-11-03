bf0dd8e2a4ccc85e6bd9e0a1a38ed99648085fb3
Steven Avon <savon40@gmail.com>
2016-11-03 15:30:20 +0000

    production.rb again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
