37de6eeae989946861148f668124842c3e9231e9
Steven Avon <savon40@gmail.com>
2016-11-03 15:55:49 +0000

    changed production agian

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
    :address              => "smtp.gmail.com",
    :port                 => 587,
    :user_name            => 'steve@cannabiznetwork.com',
-   :password             => 'cbznetwork',
+   :password             => 'cannabiz',
    :authentication       => "plain",
     :enable_starttls_auto => true
   }
