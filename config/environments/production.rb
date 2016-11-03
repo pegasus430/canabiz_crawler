5b287de12a73fdb97b01810cea473b46e06a7fbe
Steven Avon <savon40@gmail.com>
2016-11-03 15:26:51 +0000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['GMAIL_USERNAME'],
-   :password             => ENV['GMAIL_PASSWORD'],
+   :user_name            => 'steve@cannabiznetwork.com',
+   :password             => 'cbznetwork',
    :authentication       => "plain",
     :enable_starttls_auto => true
   }
