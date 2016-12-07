439edd63734cb1c73495ce421d48c66b9da3c367
Steven Avon <savon40@gmail.com>
2016-12-07 20:58:47 +0000

    removed duplicate reference to turbolinks in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require jquery.infinitescroll
 //= require turbolinks
--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,4 +1,4 @@
-	var elHeight = document.getElementById("main-title").style.height;
-	var offsetHeight = (elHeight/2) - elHeight;
-	
-	document.getElementById("main-title").style.marginTop = offsetHeight;
+var elHeight = document.getElementById("main-title").style.height;
+var offsetHeight = (elHeight/2) - elHeight;
+
+document.getElementById("main-title").style.marginTop = offsetHeight;
