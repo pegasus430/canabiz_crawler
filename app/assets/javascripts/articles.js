27b785ae7d44bec0cdc0a5e87dc0fe6e6a593eef
Steven Avon <savon40@gmail.com>
2016-12-13 02:27:22 +0000

    change method of article-body links being _blank to use javascript in article-body

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,3 +2,10 @@ var elHeight = document.getElementById("main-title").style.height;
 var offsetHeight = (elHeight/2) - elHeight;
 
 document.getElementById("main-title").style.marginTop = offsetHeight;
+
+
+//make all links within article body be target _blank 
+$(document).ready(function(){
+  $('.article-body a').attr('target', '_blank');
+});
+        
