0ee83268e94bf0e789a11f248344d8cc7a7bc306
Steven Avon <savon40@gmail.com>
2017-01-04 02:44:11 +0000

    a lot of test methods complete

--- spec/features/create_category_spec.rb
+++ spec/features/create_category_spec.rb
@@ -7,17 +7,27 @@ RSpec.feature "Create Category" do
 	end 
 	
 	scenario "An admin creates a category" do
-		login_as(@adminUser)
+		
+		#LOGIN AS THE ADMIN
+		visit "/"
+		first(:link, "Log In").click #because there is more than one
+		fill_in "Username", with: @adminUser.username
+		fill_in "Password", with: @adminUser.password
+		click_button "Log in"
+		
+		#VISIT ADMIN PAGE
 		visit "/admin"
 		expect(page).to have_link('Categories')
 		
-		#click_link "Categories"
-		#click_link "Create New Category"
+		click_link "Categories"
+		click_link "Create New Category"
 		
-		#fill_in "Name", with: "Test Category"
-		#fill_in "Keywords", with: "Test,Category"
+		fill_in "Name", with: "Test Category"
+		fill_in "Keywords", with: "Test,Category"
+		#check "Active"
 		#fill_in "Active", with: true
 		
-		#click_button "Create Category"
+		click_button "Create Category"
+		expect(page).to have_content('Category was successfully created')
 	end
 end
