aaa98d0b8bb2fd4392f850b0f0e576da5e35fad7
Steven Avon <savon40@gmail.com>
2017-04-04 15:06:53 +0000

    another sticky header fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,14 +68,14 @@ $(window).scroll(function() {
         if ($(window).scrollTop() > (header.height() - 52)) {
             stick.addClass('fix-search');
             meanBar.addClass('fix-bar');
-            $(".mobile-search-form").css({"position": "fixed"});
-            $(".mobile-search-btn").css({"position": "fixed"});
+            // $(".mobile-search-form").css({"position": "fixed"});
+            // $(".mobile-search-btn").css({"position": "fixed"});
         }
         else {
             stick.removeClass('fix-search');
             meanBar.removeClass('fix-bar');
-            $(".mobile-search-form").css({"position": "absolute"});
-            $(".mobile-search-btn").css({"position": "absolute"});
+            // $(".mobile-search-form").css({"position": "absolute"});
+            // $(".mobile-search-btn").css({"position": "absolute"});
         }
     }
 });
