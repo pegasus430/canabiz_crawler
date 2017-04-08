64ec061c9f49cd6e722f8e0a239dfbaec54454b5
Steven Avon <savon40@gmail.com>
2017-04-08 14:14:04 +0000

    another email image attempt

--- app/mailers/weekly_digest.rb
+++ app/mailers/weekly_digest.rb
@@ -5,7 +5,7 @@ class WeeklyDigest < ApplicationMailer
 	
 	def email(user)
 		@user = user
-		attachments.inline["cbz-logo-color.svg"] = File.read("#{Rails.root}/app/assets/images/header_footer/cbz-logo-color.svg")
+		attachments.inline["cbz-logo-color.svg"] = File.read("http://www.cannabiznetwork.com/assets/header_footer/cbz-logo-color.svg")
 		mail(to: @user.email, subject: 'Cannabiz Network Weekly Roll Up')
 	end
 end
