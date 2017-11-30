cb573547a95b11de782115a6f6d70371f8435522
Steven Avon <savon40@gmail.com>
2017-11-30 21:00:56 +0000

    trying one worker - canna law blog - with new_scraper_helper

--- app/jobs/news_canna_law_blog.rb
+++ app/jobs/news_canna_law_blog.rb
@@ -1,9 +1,7 @@
 class NewsCannaLawBlog < ActiveJob::Base
     include SuckerPunch::Job
     
-    
     def perform()
-    	
     	logger.info "Canna Law Blog background job is running"
         scrapeCannaLawBlog()	
     end
@@ -14,93 +12,17 @@ class NewsCannaLawBlog < ActiveJob::Base
         require 'open-uri'
         
         begin
-    		output = IO.popen(["python", "#{Rails.root}/app/scrapers/newsparser_cannalawblog.py"]) #cmd,
-        	logger.info 'BEFORE THE CONTENTS:::'
-        	contents = JSON.parse(output.read)
-        	logger.info 'HERE ARE THE CONTENTS:::::'
-        	logger.info contents
-        	if contents["articles"] != nil && contents["articles"].size > 0
-	        	addArticles(contents["articles"])
+	        output = IO.popen(["python", "#{Rails.root}/app/scrapers/newsparser_cannalawblog.py"]) #cmd,
+	        contents = JSON.parse(output.read)
+	        
+	        #call method:
+	        if contents["articles"] != nil
+	        	NewsScraperHelper.new(contents["articles"], 'Canna Law Blog').addArticles
 	        else 
 	        	ScraperError.email('CannaLawBlog News', 'No Articles were returned').deliver_now	
 	        end
-        
         rescue => ex
         	ScraperError.email('CannaLawBlog News', ex.message).deliver_now
 		end
-           	
     end
-    
-    def addArticles(articles)
-
-        @random_category = Category.where(:name => 'Random')
-        @categories = Category.where(:active => true)
-        @states = State.all
-        source = Source.find_by name: 'Canna Law Blog'
-        
-        articles.each do |article|
-        
-	        #MATCH ARTICLE CATEGORIES BASED ON KEYWORDS IN CATEGORY ARRAYS
-	        relateCategoriesSet = Set.new
-	        @categories.each do |category|
-	            if category.keywords.present?
-	                category.keywords.split(',').each do |keyword|
-	                    if  (article["title"] != nil && article["title"].downcase.include?(keyword.downcase))
-	                        relateCategoriesSet.add(category.id)
-	                        break
-	                    end
-	                end
-	            end
-	        end
-	        
-	        #MATCH ARTICLE STATES
-	        relateStatesSet = Set.new
-	        @states.each do |state|
-	            if state.keywords.present?
-	                state.keywords.split(',').each do |keyword|
-	                    #not using downcase cause i dont want to match state abbreviations that aren't capitalized
-	                    if  (article["title"] != nil && article["title"].include?(keyword))
-	                        relateStatesSet.add(state.id)
-	                        break
-	                    elsif (article["text_html"] != nil && article["text_html"].include?(keyword))
-	                    	relateStatesSet.add(state.id)
-	                    	break
-	                    end
-	                end
-	            end
-	        end
-	        
-
-
-        	#CREATE ARTICLE
-        	#missing abstract right now
-        	
-        	if article["date"] != nil
-        		article = Article.create(:title => article["title"], :remote_image_url => article["image_url"], :source_id => source.id, :date => DateTime.parse(article["date"]), :web_url => article["url"], :body => article["text_html"])	#.gsub(/\n/, '<br/><br/>')
-        	else 
-        		article = Article.create(:title => article["title"], :remote_image_url => article["image_url"], :source_id => source.id, :date => DateTime.now, :web_url => article["url"], :body => article["text_html"]) #.gsub(/\n/, '<br/><br/>')
-        	end
-
-	        
-	        #CREATE ARTICLE CATEGORIES
-	        #If no category, set category to random
-	        if relateCategoriesSet.empty?
-	           relateCategoriesSet.add(@random_category[0].id) 
-	        end
-	        
-	        relateCategoriesSet.each do |setObject|
-	            ArticleCategory.create(:category_id => setObject, :article_id => article.id)
-	        end
-	        
-	        #CREATE ARTICLE STATES
-	        relateStatesSet.each do |setObject|
-	            ArticleState.create(:state_id => setObject, :article_id => article.id)
-	        end 
-	        
-	   end #end of article loop
-	   
-	   #update last run date of scraper
-	   source.update_attribute(:last_run, DateTime.now)
-	   
-    end #end of add article method
 end
