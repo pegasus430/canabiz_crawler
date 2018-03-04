010c6616c5b9d454be5ae3a7790cb2551b3631f9
Steven Avon <savon40@gmail.com>
2018-03-04 23:58:50 +0000

    update sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,9 +1,10 @@
 class SitemapsController < ApplicationController
   def index
     @static_pages = [root_url]
-
+    @products = Product.featured
+    @vendors = Vendor.all
+    @dispensaries = Dispensary.all
     @articles = Article.all
-    @categories = Category.all
 
     respond_to do |format|
       format.xml
