5df31fd131d2456dd0458363a9943e68f679897d
Steven Avon <savon40@gmail.com>
2018-03-07 03:54:44 +0000

    fix fog errors

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,11 +2,10 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider               => "AWS",
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region                => "us-east-1"
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
     }
     
     config.fog_directory    = ENV['AWS_BUCKET']
-    # config.fog_public       = true
-    config.s3_access_policy = :public_read 
+    config.fog_public       = false
+    config.fog_attributes = { cache_control: "public, max-age=#{2000.days.to_i}" }
 end
