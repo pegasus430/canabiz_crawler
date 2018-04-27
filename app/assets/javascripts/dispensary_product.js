8200f625f3912af16534ffe6560746eb98e5835c
Steven Avon <savon40@gmail.com>
2018-04-27 22:06:13 +0000

    javascript condensing and some style condensing

--- app/assets/javascripts/dispensary_product.js
+++ app/assets/javascripts/dispensary_product.js
@@ -1,11 +1,3 @@
-// *** PRODUCT SHOW PAGE ***
-$(document).ready(function() {
-    $('#product-price-table').DataTable({
-        "scrollX": true
-    });
-});
-
-
 // ****INDEX PAGES (Mostly Product)**
 
 //when user selects an option state
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,14 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( age-verification.js )
+Rails.application.config.assets.precompile += %w( article_help.js )
+Rails.application.config.assets.precompile += %w( dispensary_product.js )
+Rails.application.config.assets.precompile += %w( sticky.js )
+Rails.application.config.assets.precompile += %w( endless_scroll.js )
+Rails.application.config.assets.precompile += %w( jquery-1.12.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery.meanmenu.js )
+Rails.application.config.assets.precompile += %w( jquery.meanmenu.min.js )
+Rails.application.config.assets.precompile += %w( jquery.scrollUp.js )
+Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( user_page.js )
